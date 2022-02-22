8abfd38d951a3696c35935b5319cb9862c606583
aimen <artem.igorevich8@rambler.ru>
2022-02-22 18:47:12 +0300

    add first user event test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('inputs should be initial empty', () => {
@@ -9,4 +10,13 @@ test('inputs should be initial empty', () => {
   expect(emailInputElement.value).toBe('');
   expect(passwordInputElement.value).toBe('');
   expect(confirmPasswordElement.value).toBe('');
+});
+
+test('should be able to type an email', () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole('textbox', {
+    name: /email/i, // we can filter all elements with role textbox by name
+  });
+  userEvent.type(emailInputElement, 'selena@gmail.com');
+  expect(emailInputElement.value).toBe('selena@gmail.com');
 });
