00bdb77de2f48dfab8b2b0b8138337a3753d977b
aimen <artem.igorevich8@rambler.ru>
2022-03-22 20:16:37 +0300

    add first two tests for our card

---
+++ pet_project/src/components/Card/__test__/Card.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import Card from '../Card';
+
+const cardProps = {
+  name: 'Sydney',
+  phone: '111-111-1111',
+  email: 'test@nuimail.com',
+  image: {
+    url: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80',
+    alt: 'cute cat',
+  },
+  favoured: false,
+};
+
+describe('Card', () => {
+  test('should show name of cat', () => {
+    render(<Card {...cardProps} />);
+    expect(
+      screen.getByRole('heading', {
+        name: /sydney/i,
+      })
+    ).toBeInTheDocument();
+  });
+
+  test('should show the phone number', () => {
+    render(<Card {...cardProps} />);
+    expect(screen.getByText(/111-111-1111/i)).toBeInTheDocument();
+  });
+
+  test('should show the email', () => {
+    render(<Card {...cardProps} />);
+    expect(screen.getByText(/test@nuimail.com/i)).toBeInTheDocument();
+  });
+});
