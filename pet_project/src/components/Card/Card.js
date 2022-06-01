553bc5d27e19c5d820058941ad7add39230f834b
aimen <artem.igorevich8@rambler.ru>
2022-06-01 18:56:34 +0300

    add component for our tests

--- pet_project/src/components/Card/Card.js
+++ pet_project/src/components/Card/Card.js
@@ -1,5 +1,18 @@
-const Card = () => {
-  return <div></div>;
+import './Card.css';
+
+const Card = ({ name, phone, email, image, favoured }) => {
+  return (
+    <div className="card">
+      <div class="card-header">
+        <img src={image.url} alt={image.alt} className="card-image" />
+      </div>
+      <div className="card-content">
+        <h3>{name}</h3>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
