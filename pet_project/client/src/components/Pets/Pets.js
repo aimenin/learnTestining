356493d22a590c142f88c698448024d0a98235e1
aimen <artem.igorevich8@rambler.ru>
2022-06-10 16:16:06 +0300

    refactor out code from local state to context api

--- pet_project/client/src/components/Pets/Pets.js
+++ pet_project/client/src/components/Pets/Pets.js
@@ -1,10 +1,15 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Filter from '../Filter/Filter';
 import Cards from '../Cards/Cards';
 
 import './Pets.css';
+
+export const PetsContext = createContext({
+  cats: [],
+  setCats: () => {},
+});
 
 const Pets = () => {
   const [cats, setCats] = useState([]);
@@ -43,8 +48,10 @@ const Pets = () => {
   return (
     <div className="container">
       <div className="app-container">
-        <Filter filters={filters} setFilters={setFilters} />
-        <Cards cats={filteredCats} setCats={setCats} />
+        <PetsContext.Provider value={{ cats: filteredCats, setCats }}>
+          <Filter filters={filters} setFilters={setFilters} />
+          <Cards />
+        </PetsContext.Provider>
       </div>
     </div>
   );
